ba8cbd575173d87edd72fd56e87535ca00887de8
Dongin Jeong <jeongdongin050416@gmail.com>
2026-05-26 21:48:00 +0900

    separate_file

--- obj/Debug/net10.0/CS20200_project.AssemblyInfo.fs
+++ obj/Debug/net10.0/CS20200_project.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("CS20200_project")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e897ee1948bc5b8d7922af0fcce02d7c0ab990")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f5981caeff3a75bfb6bbfe3d8103a555efbaf3")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("CS20200_project")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("CS20200_project")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
